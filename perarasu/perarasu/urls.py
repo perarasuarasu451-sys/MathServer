ccbe92818dd7338cc700c2fb748b25e7f1d799cf
PERARASU <perarasuarasu451@gmail.com>
2025-10-05 21:12:08 +0530

    Success

---
+++ perarasu/perarasu/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path
+from mathapp import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('areaofrightcylinder/',views.rightcylinder,name="areaofrightcylinder"),
+    path('',views.rightcylinder,name="areaofrightcylinderroot")
+]
